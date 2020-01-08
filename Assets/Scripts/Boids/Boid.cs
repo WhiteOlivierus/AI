2bbaed7f4d8804a2451836de1ce742c36170ec78
Bastien <bas.dijkstra@student.hku.nl>
2020-01-08 20:40:42 +0100

    Started with boids

---
+++ Assets/Scripts/Boids/Boid.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+
+public class Boid : MonoBehaviour
+{
+    public Vector3 velocity = Vector3.zero;
+
+    public Vector3 RuleOne(Boid[] boids)
+    {
+        // PROCEDURE rule1(boid bJ)
+
+        // 	Vector pcJ
+
+        // 	FOR EACH BOID b
+        // 		IF b != bJ THEN
+        // 			pcJ = pcJ + b.position
+        // 		END IF
+        // 	END
+
+        // 	pcJ = pcJ / N-1
+
+        // 	RETURN (pcJ - bJ.position) / 100
+
+        // END PROCEDURE
+
+        Vector3 direction = Vector3.zero;
+
+        foreach (Boid boid in boids)
+        {
+            if (boid == this) { continue; }
+
+            direction = direction + transform.position;
+        }
+
+        direction = direction / (boids.Length - 1);
+
+        return (direction - transform.position) / 100;
+    }
+    public Vector3 RuleTwo(Boid[] boids)
+    {
+        // PROCEDURE rule2(boid bJ)
+
+        // 	Vector c = 0;
+
+        // 	FOR EACH BOID b
+        // 		IF b != bJ THEN
+        // 			IF |b.position - bJ.position| < 100 THEN
+        // 				c = c - (b.position - bJ.position)
+        // 			END IF
+        // 		END IF
+        // 	END
+
+        // 	RETURN c
+
+        // END PROCEDURE
+
+        Vector3 direction = Vector3.zero;
+
+        foreach (Boid boid in boids)
+        {
+            if (boid == this) { continue; }
+
+            float distance = Vector3.Distance(boid.transform.position, transform.position);
+
+            if (distance > 100f) { continue; }
+
+            direction = direction - (boid.transform.position - transform.position);
+        }
+
+        return direction;
+    }
+    public Vector3 RuleThree(Boid[] boids)
+    {
+        // 	PROCEDURE rule3(boid bJ)
+
+        // 		Vector pvJ
+
+        // 		FOR EACH BOID b
+        // 			IF b != bJ THEN
+        // 				pvJ = pvJ + b.velocity
+        // 			END IF
+        // 		END
+
+        // 		pvJ = pvJ / N-1
+
+        // 		RETURN (pvJ - bJ.velocity) / 8
+
+        // 	END PROCEDURE
+
+        Vector3 direction = default;
+
+
+
+        return direction;
+    }
+}
---
+++ Assets/Scripts/Boids/BoidSpawner.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class BoidSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+    [SerializeField] private int amount;
+
+    private Boid[] boids;
+
+    private void Start()
+    {
+        boids = new Boid[amount];
+        for (int i = 0; i < amount; i++)
+        {
+            Vector3 position = new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height), Camera.main.farClipPlane / 2);
+            Vector3 screenPosition = Camera.main.ScreenToWorldPoint(position);
+            boids[i] = Instantiate(prefab, screenPosition, Quaternion.identity).GetComponent<Boid>();
+        }
+    }
+
+    private void Update()
+    {
+        // PROCEDURE move_all_boids_to_new_positions()
+
+        // 	Vector v1, v2, v3
+        // 	Boid b
+
+        // 	FOR EACH BOID b
+        // 		v1 = rule1(b)
+        // 		v2 = rule2(b)
+        // 		v3 = rule3(b)
+
+        // 		b.velocity = b.velocity + v1 + v2 + v3
+        // 		b.position = b.position + b.velocity
+        // 	END
+
+        // END PROCEDURE
+
+        foreach (Boid boid in boids)
+        {
+            boid.velocity = boid.velocity + boid.RuleOne(boids) + boid.RuleTwo(boids) /*+ boid.RuleThree(boids)*/;
+            boid.transform.position = boid.transform.position + boid.velocity;
+        }
+    }
+}
